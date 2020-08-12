f02742b8186d81c79816cd2f335041772c2dfe77
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-08-12 10:43:24 -0400

    Updates

---
+++ precache-manifest.13f2115d38a925aaee6e8c69c5547801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386ae8691c85101d51d4412cc664ce13",
+    "url": "/lizzRennieProject5/index.html"
+  },
+  {
+    "revision": "0fe68c578c281b8f8e10",
+    "url": "/lizzRennieProject5/static/css/main.586913a7.chunk.css"
+  },
+  {
+    "revision": "ba01a18a85b4eaf18018",
+    "url": "/lizzRennieProject5/static/js/2.194aad34.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/lizzRennieProject5/static/js/2.194aad34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe68c578c281b8f8e10",
+    "url": "/lizzRennieProject5/static/js/main.50e8df32.chunk.js"
+  },
+  {
+    "revision": "e581968a622437ddf3d4",
+    "url": "/lizzRennieProject5/static/js/runtime-main.aef5b25c.js"
+  }
+]);
