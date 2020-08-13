4919f4ecab87dd285b143a52e4dfae7735b282e7
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-08-12 20:18:49 -0400

    Update prompts

--- src/Prompts.js
+++ src/Prompts.js
@@ -12,8 +12,8 @@ const Prompts = ({ hidePrompts }) => {
         <ul>
           <li>What are your dev + career related fears?</li>
           <li>What advice can you share for other developers?</li>
-          <li>What would work be like if you had more confidence?</li>
-          <li>What achievment are you most proud of?</li>
+          <li>Tell us something about your dev journey no one knows.</li>
+          <li>What are you most proud of as a developer?</li>
         </ul>
         <button
           type="button"
